adc41bd226bd47ad49d7e7c981cf1fb0b9b2eb44
AnsariAffan007 <100890216+AnsariAffan007@users.noreply.github.com>
2024-01-20 20:05:00 +0530

    Disable cors

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(fileUpload())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
 
 mongoose.connect(process.env.MONGO_URI);
 
