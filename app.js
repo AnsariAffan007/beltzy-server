996da7d15483029ac318307912a414abdd0dae75
Mohammed Affan Ansari <100890216+AnsariAffan007@users.noreply.github.com>
2024-01-21 12:02:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(fileUpload())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     credentials: true
 }));
 
